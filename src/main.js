211b54b13c642d26490e023865767e3ca83f3269
Gboriss <galinaborissenko@gmail.com>
2020-04-17 21:07:59 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+
+window.onscroll = function() {
+    scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.backgroundColor = "#fff"
+    document.getElementById("logo-dark").style.display = "block"
+    document.getElementById("logo-light").style.display = "none"
+    document.querySelectorAll(".site-navigation a").forEach(function(e) {
+      e.style.color = "#171717"
+    })
+  
+  } else {
+    document.getElementById("logo-dark").style.display = "none"
+    document.getElementById("logo-light").style.display = "block"
+    document.getElementById("header").style.backgroundColor = "transparent"
+    document.querySelectorAll(".site-navigation a").forEach(function(e) {
+      e.style.color = "#bfbfbf"
+    })
+  }
+}
+
+let menuVisible = false
+
+let containers = document.querySelector(".containers")
+let sidenav = document.querySelector(".site-navigation")
+
+containers.addEventListener('click', function() {
+	this.classList.toggle("change")
+}) 
+
+containers.addEventListener('click', () => {
+
+  menuVisible = !menuVisible
+
+  if (menuVisible) {
+    sidenav.classList.add('visible')
+  } else {
+    sidenav.classList.remove('visible')
+  }
+})
+
+
+// containers.addEventListener('click', () => {
+
+//   containers.classList.toggle("change")
+//   sidenav.classList.toggle('visible')
+// })
+
+
+
