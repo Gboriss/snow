e94a32b24ed13bbbdddcdcc83c14ba086dedd983
Gboriss <galinaborissenko@gmail.com>
2020-04-29 19:59:06 +0300

    footer and form

--- dist/owl.carousel.min.f7f00cf3.js
+++ dist/owl.carousel.min.f7f00cf3.js
@@ -184,12 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"assets/styles/variables.scss":[function(require,module,exports) {
+},{"./bundle-url":"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"D:\\__web\\snow\\assets\\fonts\\work-sans\\work-sans-v7-latin-regular.woff":[["work-sans-v7-latin-regular.730f0e69.woff","assets/fonts/work-sans/work-sans-v7-latin-regular.woff"],"assets/fonts/work-sans/work-sans-v7-latin-regular.woff"],"D:\\__web\\snow\\assets\\fonts\\work-sans\\work-sans-v7-latin-regular.woff2":[["work-sans-v7-latin-regular.982a225a.woff2","assets/fonts/work-sans/work-sans-v7-latin-regular.woff2"],"assets/fonts/work-sans/work-sans-v7-latin-regular.woff2"],"D:\\__web\\snow\\assets\\fonts\\playfair-display\\playfair-display-v20-latin-regular.woff":[["playfair-display-v20-latin-regular.69a7da8d.woff","assets/fonts/playfair-display/playfair-display-v20-latin-regular.woff"],"assets/fonts/playfair-display/playfair-display-v20-latin-regular.woff"],"D:\\__web\\snow\\assets\\fonts\\playfair-display\\playfair-display-v20-latin-regular.woff2":[["playfair-display-v20-latin-regular.37677c29.woff2","assets/fonts/playfair-display/playfair-display-v20-latin-regular.woff2"],"assets/fonts/playfair-display/playfair-display-v20-latin-regular.woff2"],"D:\\__web\\snow\\assets\\fonts\\icon\\Pe-icon-7-stroke.woff":[["Pe-icon-7-stroke.78ae2c5e.woff","assets/fonts/icon/Pe-icon-7-stroke.woff"],"assets/fonts/icon/Pe-icon-7-stroke.woff"],"_css_loader":"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./owl.video.play.png":[["owl.video.play.72fb3377.png","OwlCarousel2-2.3.4/dist/assets/owl.video.play.png"],"OwlCarousel2-2.3.4/dist/assets/owl.video.play.png"],"_css_loader":"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52965" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57774" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/Xiaomi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/variables.1a38b8cb.js.map
+//# sourceMappingURL=/owl.carousel.min.f7f00cf3.js.map
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 
+import Flickity from 'flickity'
+
 window.onscroll = function() {
     scrollFunction()
 }
@@ -57,7 +59,7 @@ containers.addEventListener('click', () => {
 let nextPage = document.getElementById('nextPage')
 let about = document.getElementById('about')
 
-nextPage.addEventListener('click', function myFunction() {
+nextPage.addEventListener('click', function scrollIn() {
   // animate({
   //   duration: 1000,
     
@@ -68,3 +70,53 @@ nextPage.addEventListener('click', function myFunction() {
     behavior: "smooth"
   })
 })
+
+let mainCarousel = new Flickity( '.main-carousel', {
+  wrapAround: true,
+  prevNextButtons: false,
+})
+
+let logoCarousel = new Flickity( '.logo-carousel', {
+  wrapAround: true,
+  prevNextButtons: false,
+  pageDots: false,
+  autoPlay: 1500,
+
+})
+
+let blogCarousel = new Flickity( '.blog-carousel', {
+  wrapAround: true,
+  prevNextButtons: false,
+  // groupCells: true,
+  // groupCells: 3,
+})
+
+
+const ar = [2, 3, 7, 4, 6, 1, 5, 8, 9]
+// ar = [2, 1, 3, 4, 6, 5, 7, 8, 9]
+sortOdd(ar)
+
+// function sortOdd(ar) {
+//   let sortNumbers = ar.filter(x => x % 2).sort((a, b) => a - b)
+//   console.log(sortNumbers)
+//   let i = 0
+//   return ar.map(x => x % 2 ? sortNumbers[i++] : x)
+// }
+// console.log(sortOdd(ar))
+
+// const arr = [1, 4, 8, 7, 3, 6, 9, 2, 5]
+// function sortOdd(arr) {
+//   let sortNumbers = arr.filter(x => x % 2 == 0).sort((a, b) => a - b)
+  
+//   console.log(sortNumbers)
+ 
+//   let i = 0
+//   return arr.map(x => x % 2 == 0 ? sortNumbers[i++] : x)
+// }
+// console.log(sortOdd(arr))
+
+// const inputSTR = 'AAAABBBCCXYZDDDDEEEFFFAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
+// function rle(str) {
+//   let string = ''
+//   let  
+// }
